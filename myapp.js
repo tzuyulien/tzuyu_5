fee05219de43672f09b4cb7a5b5cbd4cad74a328
tzuyulien <angie981930@gmail.com>
2015-05-22 04:15:26 +0800

    3

--- myapp.js
+++ myapp.js
@@ -27,8 +27,8 @@ Get value from Bootstrap dropdown menu
 
 $('#dropdown li').on('click', function(){
     alert($(this).text());
-  $(function () {  
-  var weather,  
+ 
+  var weather, feed, title, content,  
     rss = 'http://weather.yahooapis.com/forecastrss?w=2306180&u=c',  
     url = 'https://ajax.googleapis.com/ajax/services/feed/load?v=1.0&callback=?&q=' + encodeURIComponent(rss);  
   $.ajax({  
@@ -39,19 +39,17 @@ $('#dropdown li').on('click', function(){
       console.log('oh no');  
     },  
     success: function (e) {  
-      var weather = $(e);  
+      weather = $(e);  
+      feed = weather[0].responseData.feed;  
       console.log(weather);  
-    }  
-  });  
-;  
-  });  
+      console.log(feed);  
+      title = feed.title;  
+      content = feed.entries[0].content;  
+      $('h3').append(title);  
+      $('.ui-content').append(content);  }  
+});  
  
   
-  
-
-   /* 2306179*/
-
-
-
-
+ 
+ 
 });
